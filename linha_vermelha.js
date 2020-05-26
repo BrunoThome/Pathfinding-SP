ec2f3c75a7b31e2a38dec46561836c0305554298
RomitoSamurai <lucas-araujo-007@hotmail.com>
2020-05-26 20:15:16 -0300

    Alterações Linha Vermelha

---
+++ linha_vermelha.js
@@ -0,0 +1,124 @@
+var linha vermelha ={
+	color: '#8b0000',
+        circle_color: '#0001',
+        estacoes: [
+            {
+                id: 'Mal. Deodoro',
+                name: 'Mal. Deodoro',
+                pointX: 1,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Palmeiras Barra Funda'
+                ]
+            }, {
+                id: 'Santa Cecilia',
+                name: 'Santa Cecília',
+                pointX: 1.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Mal. Deodoro',
+                    'republica'
+                ]
+            }, {
+                id: 'Anhangabau',
+                name: 'Anhangabaú',
+                pointX: 2,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'republica',
+                    'se'
+
+                ]
+            }, {
+                id: 'Pedro II',
+                name: 'Pedro II',
+                pointX: 2.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'se',
+                    'bras'
+                ]
+            }, {
+                id: 'Bresser Mooca',
+                name: 'Bresser Mooca',
+                pointX: 3,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'bras',
+                    'Belem'
+                ]
+            }, {
+                id: 'Belem',
+                name: 'Belém',
+                pointX: 3.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Bresser Mooca',
+                    'Tatuape'
+                ]
+            }, {
+                id: 'Tatuape',
+                name: 'Tatuapé',
+                pointX: 4,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Belem',
+                    'Carrao'
+                ]
+            }, {
+                id: 'Carrao',
+                name: 'Carrão',
+                pointX: 4.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Tatuape',
+                    'Penha'
+                ]
+            }, {
+                id: 'Penha',
+                name: 'Penha',
+                pointX: 5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Carrao',
+                    'Vila Matilde'
+                ]
+            }, {
+                id: 'Vila Matilde',
+                name: 'Vila Matilde',
+                pointX: 5.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Penha',
+                    'Guilhermina Esperanca'
+                ]
+            }, {
+                id: 'Guilhermina Esperanca',
+                name: 'Guilhermina-Esperança',
+                pointX: 6,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Patriarca Vila Re',
+                    'Vila Matilde'
+                ]
+            }, {
+                id: 'Patriarca Vila Re',
+                name: 'Patriarca-Vila Ré',
+                pointX: 6.5,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Guilhermina Esperanca',
+                    'Artur Alvim'
+                ]
+            }, {
+                id: 'Artur Alvim',
+                name: 'Artur Alvim',
+                pointX: 7,
+                pointY: 3,
+                estacoes_adjacentes: [
+                    'Patriarca Vila Re',
+                    'Corinthians Itaquera'
+                ]
+            }
+        ]
+}
