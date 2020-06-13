cd8b0f40d67d2a8bcb6f900581a80026baa8a07a
Bruno Thomé <brunothomee@hotmail.com>
2020-06-13 15:51:55 -0300

    Correção de posicionamento da linha diamante

--- linhas/linha-diamante.js
+++ linhas/linha-diamante.js
@@ -5,24 +5,24 @@ var linha_diamante =  {
             {
                 id: 'amador_bueno',
                 name: 'Amador Bueno',
-                pointX: -1.4,
-                pointY: -0.6,
+                pointX: -1.94,
+                pointY: -2.14,
                 estacoes_adjacentes: [
                     'santa_rita'
                 ]
             }, {
                 id: 'santa_rita',
                 name: 'Santa Rita',
-                pointX: -1.2,
-                pointY: -0.6,
+                pointX: -1.6,
+                pointY: -2.14,
                 estacoes_adjacentes: [
                     'itapevi'
                 ]
             }, {
                 id: 'itapevi',
                 name: 'Itapevi',
-                pointX: -1,
-                pointY: -0.6,
+                pointX: -1.22,
+                pointY: -2.14,
                 estacoes_adjacentes: [
                     'eng_cardoso'                    
 
@@ -30,120 +30,120 @@ var linha_diamante =  {
             }, {
                 id: 'eng_cardoso',
                 name: 'Engenheiro Cardoso',
-                pointX: -0.8,
-                pointY: -0.6,
+                pointX: -0.96,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'sagrado_coracao'
                 ]
             }, {
                 id: 'sagrado_coracao',
                 name: 'Sagrado Coracao',
-                pointX: -0.6,
-                pointY: -0.6,
+                pointX: -0.7,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'jandira'
                 ]
             }, {
                 id: 'jandira',
                 name: 'Jandira',
-                pointX: -0.4,
-                pointY: -0.6,
+                pointX: -0.46,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'jardim_silveira'
                 ]
             }, {
                 id: 'jardim_silveira',
                 name: 'Jardim Silveira',
-                pointX: -0.2,
-                pointY: -0.6,
+                pointX: -0.24,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'jardim_belval'
                 ]
             }, {
                 id: 'jardim_belval',
                 name: 'Jardim Belval',
-                pointX: 0,
-                pointY: -0.6,
+                pointX: -0.1,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'barueri'
                 ]
             }, {
                 id: 'barueri',
                 name: 'Barueri',
-                pointX: 0.2,
-                pointY: -0.6,
+                pointX: 0.1,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'antonio_joao'
                 ]
             }, {
                 id: 'antonio_joao',
                 name: 'Antonio Joao',
-                pointX: 0.4,
-                pointY: -0.6,
+                pointX: 0.28,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'santa_terezinha'
                 ]
             },{
                 id: 'santa_terezinha',
                 name: 'Santa Terezinha',
-                pointX: 0.6,
-                pointY: -0.6,
+                pointX: 0.52,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'carapicuiba'
                 ]
             },{
                 id: 'carapicuiba',
                 name: 'Carapicuiba',
-                pointX: 0.8,
-                pointY: -0.6,
+                pointX: 0.74,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'gen_miguel_costa',
                 ]
             },{
                 id: 'gen_miguel_costa',
                 name: 'General Miguel Costa',
-                pointX: 1,
-                pointY: -0.6,
+                pointX: 0.96,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'quitauna'
                 ]
             },{
                 id: 'quitauna',
                 name: 'Quitauna',
-                pointX: 1.25,
-                pointY: -0.6,
+                pointX: 1.22,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'com_sampaio'
                 ]
             },{
                 id: 'com_sampaio',
                 name: 'Comandante Sampaio',
-                pointX: 1.5,
-                pointY: -0.6,
+                pointX: 1.46,
+                pointY: -1.96,
                 estacoes_adjacentes: [
                     'osasco'
                 ]
             },{
                 id: 'imperatriz_leopoldina',
                 name: 'Imperatriz Leopoldina',
-                pointX: 2.6,
-                pointY: -0.7,
+                pointX: 2.94,
+                pointY: -2.2,
                 estacoes_adjacentes: [
                     'domingo_de_moraes'
                 ]
             },{
                 id: 'domingo_de_moraes',
                 name: 'Domingo de Moraes',
-                pointX: 2.9,
-                pointY: -0.7,
+                pointX: 3.36,
+                pointY: -2.2,
                 estacoes_adjacentes: [
                     'lapa2'
                 ]
             },{
                 id: 'lapa2',
                 name: 'Lapa 2',
-                pointX: 3.2,
-                pointY: -0.7,
+                pointX: 3.88,
+                pointY: -2.2,
                 estacoes_adjacentes: [
                     'palmeiras_barra_funda'
                     
@@ -151,10 +151,9 @@ var linha_diamante =  {
             },{
                 id: 'julio_prestes',
                 name: 'Julio Prestes',
-                pointX: 3.8,
-                pointY: -0.7,
-                estacoes_adjacentes: [
-                    'palmeiras_barra_funda',                   
+                pointX: 4.96,
+                pointY: -2.2,
+                estacoes_adjacentes: [               
                 ]
             }
         ]
